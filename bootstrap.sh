8e09d3b71d783e7b8f35352da5393000ae6d4819
tony <tony@wheezy.mynet>
2014-10-28 11:38:53 -0400

    added .config configs and did bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-# if vim existsl
+# applaunch
+git clone https://github.com/tonyfischetti/starlight.git ~/applaunch/
+
+# vim
+git clone https://github.com/tonyfischetti/vix.git ~/.vim
+ln -s ~/.vim/.vimrc ~/.vimrc
+cd ~/.vim
+git submodule init
+git submodule update
+mkdir ~/.fonts
+cp ~/.vim/goodies/Monaco+for+Powerline.otf ~/.fonts
+fc-cache -vf
+
+# zsh
+git clone https://github.com/tonyfischetti/zix.git ~/.zsh
+ln -s ~/.zsh/.zshrc ~/.zshrc
+
+# tmux
+git clone https://github.com/tonyfischetti/tmix.git ~/.tmux
+ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+
+
+
+echo "NOW REBOOT"
